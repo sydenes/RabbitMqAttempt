a54c9e087155a5209a4d2d12c9450fc0dec5a28e
sydenes <s.enessagban@gmail.com>
2024-01-20 02:00:58 +0300

    watermark project initial

---
+++ RabbitmqWatermark/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RabbitmqWatermark.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(500)]
+        public string PictureUrl { get; set; }
+        [Column(TypeName="decimal(18,2)")]
+        public decimal Price { get; set; }
+        [Range(1, 100)]
+        public int Stock { get; set; }
+    }
+}
